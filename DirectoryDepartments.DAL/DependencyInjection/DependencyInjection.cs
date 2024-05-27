39e97a399050cf92ad1bfe176b02ec5d64beef0c
FlexPudge <micha.parmuzin@yandex.ru>
2024-05-27 21:09:53 +0300

    Внедрена зависимость

---
+++ DirectoryDepartments.DAL/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using DirectoryDepartments.DAL.Repositories;
+using DirectoryDepartments.Domain.Entity;
+using DirectoryDepartments.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DirectoryDepartments.DAL.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static void AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("SqlServer");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(connectionString);
+            });
+        }
+
+        private static void InitRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<IBaseRepository<Division>, BaseRepository<Division>>();
+        }
+    }
+}
