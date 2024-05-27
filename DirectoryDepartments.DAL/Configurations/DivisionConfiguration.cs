160c8a68cb581ec8f99a26075abdea88cae0fd27
FlexPudge <micha.parmuzin@yandex.ru>
2024-05-27 21:09:35 +0300

    Добавлена конфигурация отдела

---
+++ DirectoryDepartments.DAL/Configurations/DivisionConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using DirectoryDepartments.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DirectoryDepartments.DAL.Configurations
+{
+    public class DivisionConfiguration : IEntityTypeConfiguration<Division>
+    {
+        public void Configure(EntityTypeBuilder<Division> builder)
+        {
+            builder.HasKey(d => d.Id);
+            builder.Property(d => d.Id).ValueGeneratedOnAdd();
+            builder.HasIndex(d => d.Id).IsUnique();
+            builder.HasOne(d => d.ParentDivision)
+                .WithMany(d => d.SubsidiaryDivisions)
+                .HasForeignKey(d => d.ParentId);
+            builder.Property(d => d.Name).HasMaxLength(250);
+            builder.Property(d => d.Description).HasMaxLength(250);
+        }
+    }
+}
